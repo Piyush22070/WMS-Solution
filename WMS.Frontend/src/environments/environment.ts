f825c61d284b38e5bc22a378d8560720cd9fbecc
Piyush Bhoyar <piyush@Piyushs-MacBook-Air.local>
2026-05-28 15:23:12 +0530

    fix(WMS.Frontend): update production API URL

--- WMS.Frontend/src/environments/environment.ts
+++ WMS.Frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'wms-api-prod-crhng6d8gucbgqe4.southeastasia-01.azurewebsites.net',
+  apiUrl: 'https://wms-api-prod-crhng6d8gucbgqe4.southeastasia-01.azurewebsites.net/api',
 };
